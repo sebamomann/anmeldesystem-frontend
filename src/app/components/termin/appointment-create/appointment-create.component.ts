e6a93f0822967ea6ed9f1b90514cfd75a416676f
sebamomann <github@sebamomann.de>
2020-01-05 01:13:19 +0100

    AppointmentCreate HTML template and Component refactoring

--- src/app/components/termin/appointment-create/appointment-create.component.ts
+++ src/app/components/termin/appointment-create/appointment-create.component.ts
@@ -1,9 +1,9 @@
 import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 import {FormArray, FormBuilder, FormControl, Validators} from '@angular/forms';
 import {UrlService} from '../../../services/url.service';
-import {MatAutocomplete, MatAutocompleteSelectedEvent, MatChipInputEvent, MatDialog, MatStepper} from '@angular/material';
+import {MatAutocomplete, MatAutocompleteSelectedEvent, MatChipInputEvent, MatDialog, MatSnackBar, MatStepper} from '@angular/material';
 import {COMMA, SPACE, TAB} from '@angular/cdk/keycodes';
-import {Observable, Subscription} from 'rxjs';
+import {Observable} from 'rxjs';
 import {map, startWith} from 'rxjs/operators';
 import {CreateAppointmentModel} from '../../../models/createAppointment.model';
 import {HttpClient, HttpErrorResponse, HttpEventType} from '@angular/common/http';
@@ -24,32 +24,36 @@ const HttpStatus = require('http-status-codes');
 export class AppointmentCreateComponent implements OnInit {
 
   @ViewChild('stepper', null) stepper: MatStepper;
+
   // FormGroups
   overallDataFormGroup: any;
   additionFormGroup: any;
   linkFormGroup: any;
-  doneGroup: any;
   administrationFormGroup: any;
+  fileUpload: any;
+  doneGroup: any;
 
-  /* Addition Form */
+  // Addition Form
   driverAddition = false;
 
-  /* Administration Form */
-  users: string[] = [];
-  filteredUsers: Observable<string[]>;
-
-  /* FILE UPLOAD */
-  public files: Array<{ file: File, done: boolean }> = [];
+  // Administration Form
+  administrators: string[] = [];
   allUsers: string[] = ['benutzer1@sebamomann.de', 'text@example.de', 'mama@mia.com', 'foo@bar.tld', 'hallo@helmut.rofl'];
+  filteredUsers: Observable<string[]>;
   readonly separatorKeysCodes: number[] = [COMMA, SPACE, TAB];
   @ViewChild('userInput', {static: false}) userInput: ElementRef<HTMLInputElement>;
   @ViewChild('auto', {static: false}) matAutocomplete: MatAutocomplete;
-  private fileData: IFileModelUpload[] = [];
+
+  // FileUpload
+  public files: Array<{ file: File, done: boolean }> = [];
+  private fileList: IFileModelUpload[] = [];
   private fileBlob: Blob;
+
   private percentDone = 0;
 
   constructor(private formBuilder: FormBuilder, public urlService: UrlService, private http: HttpClient, private sanitizer: DomSanitizer,
-              public dialog: MatDialog, private appointmentService: AppointmentService, private router: Router) {
+              public dialog: MatDialog, private appointmentService: AppointmentService, private router: Router,
+              private snackBar: MatSnackBar) {
 
     this.overallDataFormGroup = this.formBuilder.group({
       title: ['', Validators.required],
@@ -73,15 +77,20 @@ export class AppointmentCreateComponent implements OnInit {
       users: new FormControl()
     });
 
+    this.fileUpload = this.formBuilder.group({});
+
     this.doneGroup = this.formBuilder.group({
       saveAsTemplate: new FormControl()
     });
+  }
 
+
+  ngOnInit() {
     // Not yet working
     // noinspection TypeScriptValidateJSTypes
     this.filteredUsers = this.administrationFormGroup.get('users').valueChanges.pipe(
       startWith(null),
-      map((user: string | null) => user ? this._filter(user) : this.allUsers.slice()));
+      map((user: string | null) => user ? this._filterAdministratorAutocomplete(user) : this.allUsers.slice()));
   }
 
   async create() {
@@ -92,24 +101,20 @@ export class AppointmentCreateComponent implements OnInit {
       !this.administrationFormGroup.valid) {
       return;
     }
-    const additions = [];
-    this.additionFormGroup.controls.additions.controls.forEach(field => field.value != null ? additions.push({name: field.value}) : '');
 
-    let output: CreateAppointmentModel;
-    output = {
-      title: this.overallDataFormGroup.get('title').value,
-      description: this.linkFormGroup.get('description').value,
-      link: this.linkFormGroup.get('link').value,
-      location: this.overallDataFormGroup.get('title').value,
-      date: this.overallDataFormGroup.get('date').value,
-      deadline: this.overallDataFormGroup.get('deadline').value,
-      maxEnrollments: this.overallDataFormGroup.get('maxEnrollments').value,
-      additions,
+    const output: CreateAppointmentModel = {
+      title: this.getTitle().value,
+      description: this.getDescription().value,
+      link: this.getLink().value,
+      location: this.getLocation().value,
+      date: this.getDate().value,
+      deadline: this.getDeadline().value,
+      maxEnrollments: this.getMaxEnrollments().value,
+      additions: this.parseAdditionsFromForm(),
       driverAddition: this.driverAddition,
-      administrators: this.users,
-      files: this.fileData,
+      administrators: this.administrators,
+      files: this.fileList,
     };
-
 
     this.appointmentService
       .create(output)
@@ -117,11 +122,14 @@ export class AppointmentCreateComponent implements OnInit {
         result => {
           if (result.type === HttpEventType.UploadProgress) {
             this.percentDone = Math.round(100 * result.loaded / result.total);
-            console.log(result.loaded);
           } else if (result.type === HttpEventType.Response) {
             setTimeout(() => {
               if (result.status === HttpStatus.CREATED) {
-                this.router.navigate([`enroll`], {queryParams: {val: result.body.link}});
+                this.router.navigate([`enroll`], {
+                  queryParams: {
+                    val: result.body.link
+                  }
+                });
               }
             }, 2000);
           }
@@ -143,15 +151,8 @@ export class AppointmentCreateComponent implements OnInit {
       );
   }
 
-  ngOnInit() {
-  }
-
-  addAddition(value: string | null = null) {
-    return (this.additionFormGroup.controls.additions as FormArray).push(new FormControl(value));
-  }
-
   /* FILE UPLOAD */
-  selectFile() {
+  selectFilesFromComputer() {
     const fileUpload = document.getElementById('fileUpload') as HTMLInputElement;
     fileUpload.onchange = () => {
       // tslint:disable-next-line:prefer-for-of
@@ -159,29 +160,25 @@ export class AppointmentCreateComponent implements OnInit {
         const file = fileUpload.files[index];
         this.files.push({file, done: false});
       }
-      this.uploadFiles();
+      this.convertFiles();
     };
     fileUpload.click();
   }
 
   /* Addition Form */
-  removeAddition(index: number) {
-    this.additionFormGroup.controls.additions.removeAt(index);
-  }
-
-  hasAdditions() {
+  formHavingAdditions() {
     return this.additionFormGroup.controls.additions.controls.some(addition => addition.value !== null)
       || this.additionFormGroup.get('driverAddition').value;
   }
 
   /* Administration Form */
-  add(event: MatChipInputEvent) {
+  addAdministratorToList(event: MatChipInputEvent) {
     if (!this.matAutocomplete.isOpen) {
       const input = event.input;
       const value = event.value;
 
       if ((value || '').trim()) {
-        this.users.push(value.trim());
+        this.administrators.push(value.trim());
       }
 
       if (input) {
@@ -192,37 +189,22 @@ export class AppointmentCreateComponent implements OnInit {
     }
   }
 
-  removeUser(user): void {
-    const index = this.users.indexOf(user);
+  removeAdministratorFromList(user): void {
+    const index = this.administrators.indexOf(user);
 
     if (index >= 0) {
-      this.users.splice(index, 1);
+      this.administrators.splice(index, 1);
     }
   }
 
-  selected(event: MatAutocompleteSelectedEvent): void {
-    this.users.push(event.option.viewValue);
+  selectedAdministratorFromAutocomplete(event: MatAutocompleteSelectedEvent): void {
+    this.administrators.push(event.option.viewValue);
     this.userInput.nativeElement.value = '';
     this.administrationFormGroup.get('users').setValue(null);
   }
 
-  private _filter(value: string): string[] {
-    const filterValue = value.toLowerCase();
-
-    return this.allUsers.filter(user => user.toLowerCase().indexOf(filterValue) === 0);
-  }
-
-  async toBase64(file) {
-    return new Promise((resolve, reject) => {
-      const reader = new FileReader();
-      reader.readAsDataURL(file);
-      reader.onload = () => resolve(reader.result);
-      reader.onerror = error => reject(error);
-    });
-  }
-
-  /* Template selection*/
-  openAppointmentTemplateDialog() {
+  // Dialogs
+  public _openAppointmentTemplateDialog: () => void = () => {
     const dialogRef = this.dialog.open(TemplateDialogComponent, {
       width: '90%',
       maxWidth: 'initial',
@@ -238,58 +220,108 @@ export class AppointmentCreateComponent implements OnInit {
 
         this.linkFormGroup.get('description').setValue(result.description);
         result.additions.forEach(addition => {
-          this.addAddition(addition.name);
+          this.addAdditionFormControlToFormArray(addition.name);
         });
 
         this.driverAddition = result.driverAddition;
       }
     });
+  };
+
+  async blobToBase64(file: Blob) {
+    return new Promise((resolve, reject) => {
+      const reader = new FileReader();
+      reader.readAsDataURL(file);
+      reader.onload = () => resolve(reader.result);
+      reader.onerror = error => reject(error);
+    });
   }
 
+  // Form Util
+  addAdditionFormControlToFormArray(value: string | null = null) {
+    return (this.additionFormGroup.controls.additions as FormArray).push(new FormControl(value));
+  }
+
+  removeAdditionFromFormArray(index: number) {
+    this.additionFormGroup.controls.additions.removeAt(index);
+  }
+
+  // From values
   getLinkErrorMessage(): string {
     if (this.getLink().hasError('inUse')) {
       return 'Dieser Link ist leider schon in Benutzung';
     }
   }
 
-  private getFormGroupIndexOfLink() {
-    return 2;
+  private parseAdditionsFromForm() {
+    const additions = [];
+    this.additionFormGroup.controls.additions.controls.forEach(field => field.value != null ? additions.push({name: field.value}) : '');
+    return additions;
+  };
+
+  private _filterAdministratorAutocomplete(value: string): string[] {
+    const filterValue = value.toLowerCase();
+
+    return this.allUsers.filter(user => user.toLowerCase().indexOf(filterValue) === 0);
   }
 
-  private async uploadFile(file) {
-    this.fileBlob = new Blob([file.file], {type: 'application/octet-stream'});
-    const result = await this.toBase64(this.fileBlob).catch(e => e);
-    if (result instanceof Error) {
-      return;
-    }
-    const encoder = new TextEncoder();
-    this.fileData.push({name: file.file.name, data: result.toString()});
-    setTimeout(() => {
-        file.done = true;
-      },
-      1000);
-  }
-
-  private uploadFiles() {
+  /**
+   * Convert uploaded Files to BLOB->Bade64 String and add to array as objects for API call
+   */
+  private convertFiles() {
     const fileUpload = document.getElementById('fileUpload') as HTMLInputElement;
     fileUpload.value = '';
 
     this.files.forEach(file => {
-      this.uploadFile(file);
+      this.convertFile(file).catch(() => {
+        this.snackBar.open('Datei konnte nicht hochgeladen werden', null, {
+          duration: 2000,
+          panelClass: 'snackbar-error'
+        });
+      });
     });
+  }
+
+  private async convertFile(file) {
+    this.fileBlob = new Blob([file.file], {type: 'application/octet-stream'});
+    const result = await this.blobToBase64(this.fileBlob).catch(e => e);
+    if (result instanceof Error) {
+      return;
+    }
+    this.fileList.push({name: file.file.name, data: result.toString()});
+  }
+
+  private getMaxEnrollments() {
+    return this.overallDataFormGroup.get('maxEnrollments');
+  }
+
+  private getDeadline() {
+    return this.overallDataFormGroup.get('deadline');
+  }
+
+  private getDate() {
+    return this.overallDataFormGroup.get('date');
+  }
+
+  private getLocation() {
+    return this.overallDataFormGroup.get('location');
+  }
+
+  private getTitle() {
+    return this.overallDataFormGroup.get('title');
   }
 
   private getLink() {
     return this.linkFormGroup.get('link');
   }
-}
 
-export class FileUploadModel {
-  data: File;
-  state: string;
-  inProgress: boolean;
-  progress: number;
-  canRetry: boolean;
-  canCancel: boolean;
-  sub?: Subscription;
+  private getDescription() {
+    return this.linkFormGroup.get('description');
+  }
+
+  // Stepper Indices
+  // noinspection JSMethodCanBeStatic,JSUnusedLocalSymbols
+  private getFormGroupIndexOfLink() {
+    return 2;
+  }
 }
