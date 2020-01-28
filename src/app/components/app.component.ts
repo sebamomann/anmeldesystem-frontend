439a5e71233b2fd84f7c7792663e1d21bc638885
sebamomann <github@sebamomann.de>
2020-01-28 22:14:55 +0100

    Proper Menu

--- src/app/components/app.component.ts
+++ src/app/components/app.component.ts
@@ -6,16 +6,33 @@ import {AuthenticationService} from '../services/authentication.service';
 import {PwaService} from '../services/pwa-service.service';
 import {PwaDialogComponent} from './dialogs/pwa-dialog/pwa-dialog.component';
 import {MatDialog} from '@angular/material';
+import {animate, query, stagger, style, transition, trigger} from '@angular/animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
+  animations: [
+    trigger('listAnimation', [
+      transition('* => *', [
+        query('ul li', [
+          style({opacity: 0, transform: 'scale(0.9)'}),
+          stagger(75, [
+            animate('0.15s', style({opacity: 1, transform: 'scale(1.05)'})),
+            animate('0.075s', style({opacity: 1, transform: 'scale(1)'})),
+          ])
+        ])
+      ])
+    ])
+  ]
 })
-export class AppComponent {
 
+export class AppComponent {
+  public menu = false;
+  public items: any[];
   public now: string;
   currentUser: IUserModel;
+  private userIsLoggedIn: boolean = this.authenticationService.currentUserValue !== null;
 
   constructor(private router: Router, @Inject(WINDOW) private window: Window,
               private authenticationService: AuthenticationService, public pwa: PwaService, public dialog: MatDialog) {
@@ -42,6 +59,12 @@ export class AppComponent {
     if (pwa.promptEvent) {
       this.openPwaDialog();
     }
+
+    this.buildNav();
+  }
+
+  public toggleMenu() {
+    this.menu = !this.menu;
   }
 
   public openPwaDialog: () => void = () => {
@@ -56,5 +79,23 @@ export class AppComponent {
 
   getHostname(): string {
     return this.window.location.hostname;
+  }
+
+  buildNav() {
+    this.items = [{name: 'dashboard', redirect: '/dashboard'}];
+
+    if (this.userIsLoggedIn) {
+      this.items.push({name: 'account', redirect: '/account'});
+    }
+
+    this.items.push({name: 'kontakt'});
+    this.items.push({name: 'impressum'});
+  }
+
+  navigate(redirect: any) {
+    this.router.navigate([redirect]).then(res => {
+      const element: HTMLElement = document.getElementById('menuIcon') as HTMLElement;
+      element.click();
+    });
   }
 }
