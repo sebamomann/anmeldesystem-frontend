e31a5accd05cc751b05ef810de3a00f7dd1f34cf
sebamomann <github@sebamomann.de>
2020-02-16 17:33:34 +0100

    Provide file as download link and not as complete file

--- src/app/components/termin/appointment-data/appointment-data.component.ts
+++ src/app/components/termin/appointment-data/appointment-data.component.ts
@@ -5,6 +5,7 @@ import {IAppointmentModel} from '../../../models/IAppointment.model';
 import {Router} from '@angular/router';
 import {DomSanitizer} from '@angular/platform-browser';
 import {WINDOW} from '../../../provider/window.provider';
+import {environment} from '../../../../environments/environment';
 
 @Component({
   selector: 'app-appointment-data',
@@ -12,6 +13,8 @@ import {WINDOW} from '../../../provider/window.provider';
   styleUrls: ['./appointment-data.component.scss'],
 })
 export class AppointmentDataComponent implements OnInit {
+
+  public apiUrl = environment.api.url;
 
   @Input()
   public appointment: IAppointmentModel;
@@ -42,41 +45,8 @@ export class AppointmentDataComponent implements OnInit {
     });
   }
 
-  getBlob(base64: string) {
-    return new Blob([base64]);
-  }
-
-  arrayBufferToBase64(buffer) {
-    const TYPED_ARRAY = new Uint8Array(buffer.data);
-    return TYPED_ARRAY.reduce((data, byte) => {
-      return data + String.fromCharCode(byte);
-    }, '');
-  }
-
-
   redirectToAppointment(appointment: IAppointmentModel) {
     this.router.navigate(['/enroll'], {queryParams: {a: appointment.link}});
   }
 
-  b64toBlob(base64, type = 'application/octet-stream') {
-    // convert base64 to raw binary data held in a string
-    // doesn't handle URLEncoded DataURIs - see SO answer #6850276 for code that does this
-    // tslint:disable-next-line:prefer-const
-    let byteString = atob(base64.split(',')[1]);
-
-    // separate out the mime component
-    // tslint:disable-next-line:prefer-const
-    let mimeString = base64.split(',')[0].split(':')[1].split(';')[0];
-
-    // write the bytes of the string to an ArrayBuffer
-    const ab = new ArrayBuffer(byteString.length);
-    const ia = new Uint8Array(ab);
-    for (let i = 0; i < byteString.length; i++) {
-      ia[i] = byteString.charCodeAt(i);
-    }
-
-    // write the ArrayBuffer to a blob, and you're done
-    // tslint:disable-next-line:prefer-const
-    return new Blob([ab]);
-  }
 }
--- src/app/models/IFileModel.model.ts
+++ src/app/models/IFileModel.model.ts
@@ -1,4 +1,4 @@
 export interface IFileModel {
-  data: ArrayBuffer;
+  id: string;
   name: string;
 }
