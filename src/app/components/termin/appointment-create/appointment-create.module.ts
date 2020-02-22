3b1fd1975ad844de716c4c63b7007f52aa374578
sebamomann <github@sebamomann.de>
2020-02-22 16:40:15 +0100

    cleanup appointment-create imports

--- src/app/components/termin/appointment-create/appointment-create.module.ts
+++ src/app/components/termin/appointment-create/appointment-create.module.ts
@@ -2,26 +2,11 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {RouterModule, Routes} from '@angular/router';
 import {AppointmentCreateComponent} from './appointment-create.component';
-import {
-  MatAutocompleteModule,
-  MatButtonModule,
-  MatCardModule,
-  MatCheckboxModule,
-  MatChipsModule,
-  MatDatepickerModule,
-  MatFormFieldModule,
-  MatIconModule,
-  MatInputModule,
-  MatProgressBarModule,
-  MatStepperModule,
-  MatTooltipModule
-} from '@angular/material';
-import {ReactiveFormsModule} from '@angular/forms';
-import {NgxMatDatetimePickerModule} from 'ngx-mat-datetime-picker';
 import {AuthGuard} from '../../../_helper/auth.guard';
-import {ApplicationPipesModule} from '../../../pipes/application-pipes.module';
+import {MatButtonModule, MatCardModule, MatIconModule, MatProgressBarModule, MatStepperModule} from '@angular/material';
 import {OverallDataModule} from '../form/overall-data/overall-data.module';
 import {AdditionsModule} from '../form/additions/additions.module';
+import {ReactiveFormsModule} from '@angular/forms';
 
 const routes: Routes = [
   {path: '', component: AppointmentCreateComponent, canActivate: [AuthGuard]}
@@ -36,21 +21,12 @@ const routes: Routes = [
     RouterModule.forChild(routes),
     MatCardModule,
     MatStepperModule,
-    ReactiveFormsModule,
-    NgxMatDatetimePickerModule,
-    MatFormFieldModule,
-    MatDatepickerModule,
-    MatCheckboxModule,
-    MatIconModule,
-    MatChipsModule,
-    MatAutocompleteModule,
-    MatProgressBarModule,
-    MatTooltipModule,
-    MatInputModule,
-    MatButtonModule,
-    ApplicationPipesModule,
     OverallDataModule,
     AdditionsModule,
+    MatButtonModule,
+    ReactiveFormsModule,
+    MatIconModule,
+    MatProgressBarModule,
   ],
   exports: [],
   providers: []
