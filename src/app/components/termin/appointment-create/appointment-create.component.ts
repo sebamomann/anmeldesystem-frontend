9243a3f1c197561af3de153d35bdfe33fb4611a0
sebamomann <github@sebamomann.de>
2020-02-23 11:31:10 +0100

    appointment create now properly working

--- src/app/components/termin/appointment-create/appointment-create.component.ts
+++ src/app/components/termin/appointment-create/appointment-create.component.ts
@@ -1,7 +1,7 @@
 import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 import {FormBuilder, FormControl} from '@angular/forms';
 import {MatAutocomplete, MatDialog, MatStepper} from '@angular/material';
-import {HttpClient} from '@angular/common/http';
+import {HttpClient, HttpErrorResponse, HttpEventType} from '@angular/common/http';
 import {DomSanitizer} from '@angular/platform-browser';
 import {AppointmentService} from '../../../services/appointment.service';
 import {Router} from '@angular/router';
@@ -41,61 +41,43 @@ export class AppointmentCreateComponent implements OnInit {
 
   async create() {
     console.log(JSON.stringify(this.output));
-    // if (!this.overallDataFormGroup.valid ||
-    //   !this.additionFormGroup.valid ||
-    //   !this.linkFormGroup.valid ||
-    //   !this.doneGroup.valid ||
-    //   !this.administrationFormGroup.valid) {
-    //   return;
-    // }
-    //
-    // const output: CreateAppointmentModel = {
-    //   title: this.getTitle().value,
-    //   description: this.getDescription().value,
-    //   link: this.getLink().value,
-    //   location: this.getLocation().value,
-    //   date: this.getDate().value,
-    //   deadline: this.getDeadline().value,
-    //   maxEnrollments: this.getMaxEnrollments().value,
-    //   additions: this.parseAdditionsFromForm(),
-    //   driverAddition: this.getDriverAddition().value,
-    //   administrators: this.administrators,
-    //   files: this.fileList,
-    // };
-    //
-    // this.appointmentService
-    //   .create(output)
-    //   .subscribe(
-    //     result => {
-    //       if (result.type === HttpEventType.UploadProgress) {
-    //         this.percentDone = Math.round(100 * result.loaded / result.total);
-    //       } else if (result.type === HttpEventType.Response) {
-    //         setTimeout(() => {
-    //           if (result.status === HttpStatus.CREATED) {
-    //             this.router.navigate([`enroll`], {
-    //               queryParams: {
-    //                 a: result.body.link
-    //               }
-    //             });
-    //           }
-    //         }, 2000);
-    //       }
-    //     },
-    //     (err: HttpErrorResponse) => {
-    //       if (err.status === HttpStatus.BAD_REQUEST) {
-    //         if (err.error.code === 'DUPLICATE_ENTRY') {
-    //           err.error.error.forEach(fColumn => {
-    //               const uppercaseName = fColumn.charAt(0).toUpperCase() + fColumn.substring(1);
-    //               const fnName: string = 'get' + uppercaseName;
-    //               this[fnName]().setErrors({inUse: true});
-    //               const fnNameForIndex = 'getFormGroupIndexOf' + uppercaseName;
-    //               this.stepper.selectedIndex = this[fnNameForIndex]();
-    //             }
-    //           );
-    //         }
-    //       }
-    //     }
-    //   );
+
+    this.output.administrators = [];
+    this.output.files = [];
+
+    this.appointmentService
+      .create(this.output)
+      .subscribe(
+        result => {
+          if (result.type === HttpEventType.UploadProgress) {
+            this.percentDone = Math.round(100 * result.loaded / result.total);
+          } else if (result.type === HttpEventType.Response) {
+            setTimeout(() => {
+              if (result.status === HttpStatus.CREATED) {
+                this.router.navigate([`enroll`], {
+                  queryParams: {
+                    a: result.body.link
+                  }
+                });
+              }
+            }, 2000);
+          }
+        },
+        (err: HttpErrorResponse) => {
+          if (err.status === HttpStatus.BAD_REQUEST) {
+            if (err.error.code === 'DUPLICATE_ENTRY') {
+              err.error.error.forEach(fColumn => {
+                  const uppercaseName = fColumn.charAt(0).toUpperCase() + fColumn.substring(1);
+                  const fnName: string = 'get' + uppercaseName;
+                  this[fnName]().setErrors({inUse: true});
+                  const fnNameForIndex = 'getFormGroupIndexOf' + uppercaseName;
+                  this.stepper.selectedIndex = this[fnNameForIndex]();
+                }
+              );
+            }
+          }
+        }
+      );
   }
 
   // Dialogs
