584fd3b518b27b77d0421342f26837f9e7bd5477
sebamomann <github@sebamomann.de>
2020-03-02 20:35:52 +0100

    allow to access dashboard, also if not logged in

--- src/app/components/termin/dashboard/dashboard.module.ts
+++ src/app/components/termin/dashboard/dashboard.module.ts
@@ -2,12 +2,11 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {RouterModule, Routes} from '@angular/router';
 import {DashboardComponent} from './dashboard.component';
-import {AuthGuard} from '../../../_helper/auth.guard';
 import {MatButtonModule, MatCardModule, MatFormFieldModule, MatIconModule, MatInputModule, MatProgressBarModule} from '@angular/material';
 import {AppointmentDataModule} from '../appointment-data/appointment-data.module';
 
 const routes: Routes = [
-  {path: '', component: DashboardComponent, canActivate: [AuthGuard]}
+  {path: '', component: DashboardComponent}
 ];
 
 @NgModule({
