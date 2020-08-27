f779c77190ed5f54708fb0d2e6caaeabfb241cc2
sebamomann <github@sebamomann.de>
2020-08-27 22:42:39 +0200

    after logout navigate to dashboard

--- src/app/components/account/main/logout/logout.component.ts
+++ src/app/components/account/main/logout/logout.component.ts
@@ -14,6 +14,6 @@ export class LogoutComponent implements OnInit {
 
   ngOnInit() {
     this.authService.logout();
-    this.router.navigate(['/account/login']);
+    this.router.navigate(['/dashboard']);
   }
 }
