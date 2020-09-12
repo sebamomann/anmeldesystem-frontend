59712fab9cd8955577309eb6193be360ba2f67c3
sebamomann <github@sebamomann.de>
2020-09-12 22:08:15 +0200

    replace in prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  API_URL: window['env']['API_URL'] || 'http://localhost:3001/',
+  API_URL: window['env']['API_URL'] || 'http://localhost:34250/',
 };
