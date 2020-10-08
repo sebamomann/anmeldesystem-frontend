03d49df86643d506a1f72e83f0fbe20c0208880d
sebamomann <github@sebamomann.de>
2020-10-08 18:02:30 +0200

    .exe test

--- e2e/protractor.conf.js
+++ e2e/protractor.conf.js
@@ -23,7 +23,7 @@ exports.config = {
     },
     browserName: "chrome",
   },
-  chromeDriver: '../node_modules/protractor/node_modules/webdriver-manager/selenium/chromedriver_85.0.4183.121',
+  chromeDriver: '../node_modules/protractor/node_modules/webdriver-manager/selenium/chromedriver_85.0.4183.121.exe',
   directConnect: true,
   baseUrl: "http://localhost:4200",
   framework: "jasmine",
